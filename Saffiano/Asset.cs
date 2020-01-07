14b60052cd8af9684111e0323d4cdaa0d2d984e5
Wu Zhiwei <503098961@QQ.com>
2020-01-08 00:59:51 +0800

    Read *.PNG texture with FreeImage

---
+++ Saffiano/Asset.cs
@@ -0,0 +1,16 @@
+﻿using System.IO;
+
+namespace Saffiano
+{
+    public class Asset
+    {
+        protected string extension = null;
+        protected string filePath = null;
+
+        public Asset(string filePath)
+        {
+            this.filePath = filePath;
+            extension = Path.GetExtension(filePath).ToUpper();
+        }
+    }
+}
---
+++ Saffiano/Texture.cs
@@ -0,0 +1,63 @@
+﻿using FreeImageAPI;
+using System;
+
+namespace Saffiano
+{
+    public class Texture : Asset
+    {
+        public Color[] pixels
+        {
+            get;
+            private set;
+        }
+
+        public uint width
+        {
+            get;
+            private set;
+        }
+
+        public uint height
+        {
+            get;
+            private set;
+        }
+
+        public Texture(string filePath) : base(filePath)
+        {
+            pixels = null;
+            width = 0;
+            height = 0;
+            switch (extension)
+            {
+                case ".PNG":
+                    PNG();
+                    break;
+                default:
+                    throw new NotImplementedException();
+            }
+        }
+
+        private void PNG()
+        {
+            FIBITMAP bitmap = FreeImage.LoadEx(filePath);
+            width = FreeImage.GetWidth(bitmap);
+            height = FreeImage.GetHeight(bitmap);
+            pixels = new Color[height * width];
+            for (uint y = 0; y < height; y++)
+            {
+                for (uint x = 0; x < width; x++)
+                {
+                    RGBQUAD value;
+                    FreeImage.GetPixelColor(bitmap, x, y, out value);
+                    uint index = y * width + x;
+                    pixels[index].r = value.rgbRed / 255.0f;
+                    pixels[index].g = value.rgbGreen / 255.0f;
+                    pixels[index].b = value.rgbBlue / 255.0f;
+                    pixels[index].a = value.rgbReserved / 255.0f;
+                }
+            }
+            FreeImage.UnloadEx(ref bitmap);
+        }
+    }
+}
