946a06438f7ff0c0a93c3345487d9c34180ffa22
Wu Zhiwei <503098961@QQ.com>
2021-11-07 03:09:01 +0800

    Adjust bunny's model inner position

--- Saffiano.Gallery/Assets/Objects/Bunny.cs
+++ Saffiano.Gallery/Assets/Objects/Bunny.cs
@@ -10,6 +10,8 @@ namespace Saffiano.Gallery.Assets.Objects
 
         private Material _material = new Resources.Default.Material.Phong();
 
+        private Transform pivot = null;
+
         public Material material
         {
             get => _material;
@@ -31,13 +33,16 @@ namespace Saffiano.Gallery.Assets.Objects
             this.name = "Bunny";
             this.AddComponent<Transform>();
 
+            this.pivot = new GameObject().AddComponent<Transform>();
+            this.pivot.localPosition = new Vector3(0, -0.033f, 0);
+
             // LOD0
             GameObject lod0 = new GameObject("Bunny.LOD0");
             lod0.AddComponent<Transform>();
             lod0.AddComponent<MeshFilter>();
             lod0.AddComponent<MeshRenderer>().material = _material;
             lod0.AddComponent<MeshAsyncLoader>().path = "models/bunny/reconstruction/bun_zipper.ply";
-            lod0.transform.parent = this.transform;
+            lod0.transform.parent = this.pivot.transform;
             lods.Add(new LOD(0.60f, new Renderer[] { lod0.GetComponent<Renderer>() }));
 
             // LOD1
@@ -46,7 +51,7 @@ namespace Saffiano.Gallery.Assets.Objects
             lod1.AddComponent<MeshFilter>();
             lod1.AddComponent<MeshRenderer>().material = _material;
             lod1.AddComponent<MeshAsyncLoader>().path = "models/bunny/reconstruction/bun_zipper_res2.ply";
-            lod1.transform.parent = this.transform;
+            lod1.transform.parent = this.pivot.transform;
             lods.Add(new LOD(0.30f, new Renderer[] { lod1.GetComponent<Renderer>() }));
 
             // LOD2
@@ -55,7 +60,7 @@ namespace Saffiano.Gallery.Assets.Objects
             lod2.AddComponent<MeshFilter>();
             lod2.AddComponent<MeshRenderer>().material = _material;
             lod2.AddComponent<MeshAsyncLoader>().path = "models/bunny/reconstruction/bun_zipper_res3.ply";
-            lod2.transform.parent = this.transform;
+            lod2.transform.parent = this.pivot.transform;
             lods.Add(new LOD(0.10f, new Renderer[] { lod2.GetComponent<Renderer>() }));
 
             this.AddComponent<LODGroup>().SetLODs(lods.ToArray());
